dbb7a0bb5c41d692afbd40f367002fb2ef170a11
Harsika Kumari <kumariharsika8@gmail.com>
2024-09-13 00:01:42 +0530

    practising some array questions in php

---
+++ CSE57D/array/range.php
@@ -0,0 +1,9 @@
+<?php
+/* generate the unique random number within the range.*/
+
+//$range=array(11,20);
+$a=range(11,20);
+echo shuffle($a);
+print_r ($a);
+
+?>
---
+++ CSE57D/array/reset.php
@@ -0,0 +1,5 @@
+<?php
+$a=array(4=>'white',6=>'green',11=>'red');
+$a=reset($a);
+print_r($a);
+?>
