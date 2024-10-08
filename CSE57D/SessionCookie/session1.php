d47e6ef79a771d6f7045d5c2e2ca948adc03e993
Harsika Kumari <kumariharsika8@gmail.com>
2024-10-08 23:16:07 +0530

    session practise in PHP

---
+++ CSE57D/SessionCookie/session1.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+?>
+<html>
+<body>
+<?php
+//Set session variables
+$_SESSION["Username"]="Anchal Jaiswal";
+$_SESSION["Password"]="Anch09";
+echo "variables are set";
+?>
+</body>
+</html>
